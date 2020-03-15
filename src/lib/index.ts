926921c60df8f061221180340072082e00999c5f
devniel <dnielfs@gmail.com>
2020-03-15 23:13:27 +0100

    Fixing dependencies and exposing classes

--- src/lib/index.ts
+++ src/lib/index.ts
@@ -1,3 +1,23 @@
 import { TypeOrmTestModule } from './typeorm-test.module';
+import {
+  createFakeConnection,
+  fakeConnection,
+  getFakeConnectionManager,
+} from './createFakeConnection';
+import { FakeConnection } from './FakeConnection';
+import { FakeConnectionManager } from './FakeConnectionManager';
+import { FakeDBDriver } from './FakeDBDriver';
+import { FakeDriverFactory } from './FakeDriverFactory';
+import { FakePostgresQueryRunner } from './FakePostgresQueryRunner';
 
-export { TypeOrmTestModule };
+export {
+  TypeOrmTestModule,
+  createFakeConnection,
+  fakeConnection,
+  getFakeConnectionManager,
+  FakeConnection,
+  FakeConnectionManager,
+  FakeDBDriver,
+  FakeDriverFactory,
+  FakePostgresQueryRunner,
+};
